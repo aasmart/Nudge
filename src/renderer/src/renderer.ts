55d87b7bb08184051cffe295aa5344aa9e693582
aasmart <48771274+aasmart@users.noreply.github.com>
2023-07-01 10:47:58 -0400

    * Fix dialog word wrap issue * Updated method for showing the popup

--- src/renderer/src/renderer.ts
+++ src/renderer/src/renderer.ts
@@ -63,7 +63,7 @@ function listReminders() {
             const index = Reminders.activeReminders.indexOf(reminder)
             if(index < 0) {
                 console.error("Failed to edit reminder for it does not exist")
-                sendPopup('Encountered An Error', 'An error was encounter while trying to edit the reminder')
+                showPopup('Encountered An Error', 'An error was encounter while trying to edit the reminder')
                 return;
             }
 
@@ -127,41 +127,15 @@ function listReminders() {
     reminderList.replaceChildren(...reminders)
 }
 
-function sendPopup(title: string, content: string) {
-    // const popupContainer = document.getElementsByClassName("popup-container")[0] as HTMLElement
+function showPopup(title: string, content: string) {
+    const popup = document.getElementById("popup") as HTMLDialogElement;
+    const titleElement = popup.getElementsByClassName("title")[0] as HTMLElement;
+    const bodyElement = popup.getElementsByClassName("content")[0] as HTMLElement;
 
-    // if(popupContainer === null) {
-    //     console.error('Cannot create popup as the container does not exist')
-    //     return;
-    // }
+    titleElement.innerText = title;
+    bodyElement.innerText = content;
 
-    // const section = popupContainer.children[0] as HTMLElement
-    // const popupTitle = section.children[0] as HTMLElement
-    // const popupText = section.children[1] as HTMLElement
-    // const popupButton = section.children[2] as HTMLButtonElement
-
-    // popupTitle.innerText = title
-    // popupText.innerText = content
-
-    // function handleButton() {
-    //     section.classList.remove('show-popup')
-    //     section.classList.add('hide-popup')
-    //     popupButton.style.visibility = 'hidden'
-    // }
-
-    // function hideContainer(e: AnimationEvent) {
-    //     if(e.animationName === 'popup-out')
-    //         popupContainer.style.visibility = 'hidden'    
-    // }
-
-    // popupButton.addEventListener('click', handleButton)
-    // popupContainer.addEventListener('animationend', hideContainer)
-
-    // // Show the popup
-    // popupContainer.style.visibility = 'visible'
-    // popupButton.style.visibility = 'visible'
-    // section.classList.remove('hide-popup')
-    // section.classList.add('show-popup')
+    popup.showModal();
 }
 
 function loadCreateRemindersPage() {

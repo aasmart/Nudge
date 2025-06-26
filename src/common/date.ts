f975383fa59e4a421e850b7119c3682b6b0bb9ce
aasmart <alexandersmart2005@gmail.com>
2025-06-26 15:38:33 -0400

    Fix relative time being 1 min less

--- src/common/date.ts
+++ src/common/date.ts
@@ -6,7 +6,7 @@ declare global {
     }
 }
 
-Date.prototype.addMilliseconds = function(milliseconds: number): Date {
+Date.prototype.addMilliseconds = function (milliseconds: number): Date {
     const date = this;
     return new Date(date.getTime() + milliseconds)
 }
@@ -14,16 +14,16 @@ Date.prototype.addMilliseconds = function(milliseconds: number): Date {
 module DateUtils {
     export function getTimeDifferenceString(a: Date, b: Date): string {
         const difference = b.valueOf() - a.valueOf();
-        if(difference < 0) {
+        if (difference < 0) {
             throw new EvalError("Date b must be after date a");
-        } else if(difference >= Constants.DAY_MS) {
-            const days = Math.floor(difference / Constants.DAY_MS);
+        } else if (difference >= Constants.DAY_MS) {
+            const days = Math.ceil(difference / Constants.DAY_MS);
             return `<${days} ${days == 1 ? "day" : "days"}.`;
-        } else if(difference >= Constants.HOUR_MS) {
-            const hours = Math.floor(difference / Constants.HOUR_MS);
+        } else if (difference >= Constants.HOUR_MS) {
+            const hours = Math.ceil(difference / Constants.HOUR_MS);
             return `<${hours} ${hours == 1 ? "hour" : "hours"}.`;
-        } else if(difference >= Constants.MINUTES_TO_MS) {
-            const minutes = Math.floor(difference / Constants.MINUTES_TO_MS);
+        } else if (difference >= Constants.MINUTES_TO_MS) {
+            const minutes = Math.ceil(difference / Constants.MINUTES_TO_MS);
             return `<${minutes} ${minutes == 1 ? "minute" : "minutes"}.`;
         }
         return "<1 minute.";

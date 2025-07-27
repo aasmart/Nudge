9b4b837ed1db966e007c51b67cadd0422fc98e65
aasmart <alexandersmart2005@gmail.com>
2025-07-26 23:06:59 -0400

    fix: contents of modal initally hidden

--- src/renderer/src/modal.ts
+++ src/renderer/src/modal.ts
@@ -34,6 +34,8 @@ window.addEventListener("load", () => {
     const modal = document.getElementsByClassName("modal")[0];
 
     window.api.getModalParams().then(res => {
+        modal.setAttribute("visible", "false");
+
         const templatePrefix = `modal__${res.template}`;
         const template = document.getElementById(`${res.template}-template`) as HTMLTemplateElement | null;
 
@@ -71,6 +73,8 @@ window.addEventListener("load", () => {
                 element.innerText = `${value}`;
             }
         });
+
+        modal.setAttribute("visible", "true");
     });
 
     button?.addEventListener("click", () => {
